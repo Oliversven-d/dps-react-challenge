f3df53f3c1d1c7ebcc9b15828e8abc31b373366d
Your Name <you@example.com>
2025-04-23 15:32:56 +0200

    added working Userlist component with necessary changes

--- src/Userlist.tsx
+++ src/Userlist.tsx
@@ -8,17 +8,15 @@ type User = {
   address: {
     city: string;
   };
+  isOldestInCity?: boolean;
+  isMatch?: boolean;
 };
 
 type Props = {
-  users: User[]; 
   filteredUsers: User[]; 
 };
 
-const UserList: React.FC<Props> = ({ users, filteredUsers }) => {
-  const isFiltered = (user: User) =>
-    filteredUsers.some((u) => u.id === user.id);
-
+const UserList: React.FC<Props> = ({ filteredUsers }) => {
   return (
     <div
       style={{
@@ -42,30 +40,29 @@ const UserList: React.FC<Props> = ({ users, filteredUsers }) => {
         <div style={{ flex: 1, color: 'black' }}>Birthday</div>
       </div>
 
-      {users.map((user) => {
-        const highlight = isFiltered(user);
+      {filteredUsers.map((user) => {
+        const isMatch = user.isMatch;
+        const isOldest = user.isOldestInCity;
 
+        const rowStyle: React.CSSProperties = {
+          display: 'flex',
+          padding: '0.75rem 0',
+          borderBottom: '1px solid #ddd',
+          borderRadius: '4px',
+          marginBottom: '0.25rem',
+          backgroundColor: isOldest ? '#007bff' : isMatch ? '#d0ebff' : 'transparent', 
+          color: isOldest ? 'white' : 'black',  
+        
+        };
         return (
-          <div
-            key={user.id}
-            style={{
-              display: 'flex',
-              padding: '0.75rem 0', 
-              backgroundColor: highlight ? '#d0ebff' : undefined,
-              borderBottom: '1px solid #ddd',
-              borderRadius: '4px',
-              marginBottom: '0.25rem',
-            }}
-          >
-            <div style={{ flex: 1, paddingRight: '1rem', color: 'black' }}>
+          <div key={user.id} style={rowStyle}>
+            <div style={{ flex: 1, paddingRight: '1rem' }}>
               {user.firstName} {user.lastName}
             </div>
-            <div style={{ flex: 1, paddingRight: '1rem', color: 'black' }}>
+            <div style={{ flex: 1, paddingRight: '1rem' }}>
               {user.address.city}
             </div>
-            <div style={{ flex: 1, color: 'black' }}>
-              {user.birthDate}
-            </div>
+            <div style={{ flex: 1 }}>{user.birthDate}</div>
           </div>
         );
       })}
@@ -74,3 +71,5 @@ const UserList: React.FC<Props> = ({ users, filteredUsers }) => {
 };
 
 export default UserList;
+
+
